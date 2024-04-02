ee340cd7cccf0953f480142cf8fc315a82355544
ReaperGoku <58998275+ReaperGoku@users.noreply.github.com>
2024-04-02 07:20:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import platform
+
+from modules import *
+from widgets import *
+os.environ["QT_FONT_DPI"] = "96" # FIX Problem for High DPI and Scale above 100%
+
+# SET AS GLOBAL WIDGETS
+# ///////////////////////////////////////////////////////////////
+widgets = None
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        # SET AS GLOBAL WIDGETS
+        # ///////////////////////////////////////////////////////////////
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        global widgets
+        widgets = self.ui
+
+        # ///////////////////////////////////////////////////////////////
+        Settings.ENABLE_CUSTOM_TITLE_BAR = True
+
+        # SET UI DEFINITIONS
+        # ///////////////////////////////////////////////////////////////
+        UIFunctions.uiDefinitions(self)
+
+        # SHOW APP
+        # ///////////////////////////////////////////////////////////////
+        self.show()
+
+    # RESIZE EVENTS
+    # ///////////////////////////////////////////////////////////////
+    def resizeEvent(self, event):
+        # Update Size Grips
+        UIFunctions.resize_grips(self)
+
+    # MOUSE CLICK EVENTS
+    # ///////////////////////////////////////////////////////////////
+    def mousePressEvent(self, event):
+        # SET DRAG POS WINDOW
+        self.dragPos = event.globalPosition().toPoint()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("icon.ico"))
+    window = MainWindow()
+    sys.exit(app.exec())
